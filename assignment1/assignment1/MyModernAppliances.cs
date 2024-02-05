a9823b06cb78ac22f18f5c4a93b9d515d74c5dcf
Adam Nordquist <apnordquist@gmail.com>
2024-02-05 14:24:35 -0700

    added dishwasher and refirgereator display

--- assignment1/assignment1/MyModernAppliances.cs
+++ assignment1/assignment1/MyModernAppliances.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 
 namespace assignment1
 {
@@ -23,14 +24,52 @@ namespace assignment1
                         appliance.Quantity--; //if available decrease quantity by one
                         Console.WriteLine(appliance.ToString()); //display appliance info
                     }
-                            }
+                }
             else { Console.WriteLine("Appliance not available"); } //otherwise, display appliance is not
             }
         }
         public override void DisplayDishwashers()
         {
-           //prompt user to enter a field (like sound rating)
-           //return details of all appliances that match
+            //prompt user to enter a field (like sound rating)
+            List<Appliance> dishwasherList = new List<Appliance>();
+            string searchSound = "";
+            Console.WriteLine("Which sound rating:\n" +
+                "1 - Moderate\n" +
+                "2 - Quiet\n" +
+                "3 - Quieter\n" +
+                "4 - Quietest\n" +
+                "Enter selection:\n");
+            int soundSelect = int.Parse(Console.ReadLine());
+            switch(soundSelect) 
+            {
+                case 1:
+                    searchSound = "M";
+                    break;
+                case 2:
+                    searchSound = "Qu";
+                    break;
+                case 3:
+                    searchSound = "Qr";
+                    break;
+                case 4:
+                    searchSound = "Qt";
+                    break;
+                default:
+                    Console.WriteLine("Selection not found.");
+                    break;
+            }
+            foreach (Appliance appliance in Appliances) // put dishwasher in list based on criteria
+            {
+                if (searchSound == ((Dishwasher)appliance).SoundRating)
+                {
+                    dishwasherList.Add(appliance);
+                }
+            }
+            //return details of all appliances that match
+            foreach(Appliance appliance in dishwasherList)
+            {
+                Console.WriteLine(appliance.ToString() );
+            }
         }
         public override void DisplayMicrowaves()
         {
@@ -38,7 +77,22 @@ namespace assignment1
         }
         public override void DisplayRefrigerator()
         {
-            
+            List<Appliance> refrigeratorList = new List<Appliance>();
+            string searchDoors = "";
+            Console.WriteLine("How many doors:\n";
+            int doorSelect = int.Parse(Console.ReadLine());
+            foreach (Appliance appliance in Appliances) // put dishwasher in list based on criteria
+            {
+                if (doorSelect == ((Refrigerator)appliance).Doors)
+                {
+                    refrigeratorList.Add(appliance);
+                }
+            }
+            //return details of all appliances that match
+            foreach (Appliance appliance in refrigeratorList)
+            {
+                Console.WriteLine(appliance.ToString());
+            }
         }
         public override void DisplayVacuums()
         {
@@ -46,7 +100,7 @@ namespace assignment1
         }
         public override void Find()
         {
-            List<Appliance> brandQuery = new List<Appliance>();
+            List<Appliance> brandQuery = new List<Appliance>(); //new list for the brand quarry
             Console.WriteLine("Please enter the brand you are searching for:\n");
             string query = Console.ReadLine(); //prompt user to enter a brand
             foreach (Appliance appliance in Appliances)
@@ -63,8 +117,33 @@ namespace assignment1
         }
         public override void RandomList()
         {
+            List<Appliance> randomAppliances = new List<Appliance>(); //new list for the random selection
+            //prompt user to enter type of appliances
+            Console.WriteLine("Appliance Types:\n" +
+                "1 - Refirgerators\n" +
+                "2 - Vacuums\n" +
+                "3 - Microwaves\n" +
+                "4 - Dishwashers\n" +
+                "Enter type of type of appliance:\n");
+            char selectionRandom = char.Parse(Console.ReadLine());
             //prompt user to enter a number of appliances
+            Console.WriteLine("How many appliances:\n");
+            int numberRandom = int.Parse(Console.ReadLine());
             //return that many random appliances using random comparer
+            for (int i = 0; i < numberRandom; i++) //where to add randomcomaparer?
+            {
+                foreach (Appliance appliance in Appliances)
+                {
+                    if (selectionRandom == appliance.Number[0];
+                    {
+                        randomAppliances.Add(appliance);
+                    }
+                }
+            }
+            foreach (Appliance appliance in randomAppliances)
+            {
+                Console.WriteLine(appliance.ToString);
+            }
         }
     }
 }
